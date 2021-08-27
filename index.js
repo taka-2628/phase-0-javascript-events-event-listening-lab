55dffa42609d7cf94743a212fed58ff660d400f1
Takashi <th2826@columbia.edu>
2021-08-27 00:10:18 -0400

    Completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
-function addingEventListener() {
+//Write a function that
+//      adds an event listener to input element
+//      when clicked, triggers alert 'I was clicked
+
+
+//callback: anonymous function: solution
+function addingEventListener(){
     const input = document.getElementById('input');
-    input.addEventListener('click',function() {
-        alert('I was clicked!');
-    }); 
+    input.addEventListener('click', function(){alert('I was clicked')});
 }
 
-addingEventListener();
+//callback: arrow function
+function addingEventListenerArr(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', () => alert('I was clicked'));
+}
+
